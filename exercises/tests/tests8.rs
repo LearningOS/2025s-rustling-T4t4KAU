35d927b6c7302e5f8c8e6b12d1a018a888f3ceeb
T4t4KAU <huangwenxuan271828@163.com>
2025-04-05 23:16:57 +0800

    update

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -1,12 +1,11 @@
 // tests8.rs
 //
-// This execrise shares `build.rs` with the previous exercise.
+// This exercise shares `build.rs` with the previous exercise.
 // You need to add some code to `build.rs` to make both this exercise and
 // the previous one work.
 //
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
-
 
 fn main() {}
 
@@ -17,8 +16,13 @@ mod tests {
     #[test]
     fn test_success() {
         #[cfg(feature = "pass")]
-        return;
+        {
+            return;
+        }
 
-        panic!("no cfg set");
+        #[cfg(not(feature = "pass"))]
+        {
+            panic!("no cfg set");
+        }
     }
 }
